9ca7513eded52d51fae8854278a246c7d1ad62bb
chaikaa <1>
2022-12-13 21:30:09 +0300

    L16

--- L16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ L16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,14 +1,31 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class FileSerializer implements Serializer {
 
+    private final String fileName;
+
     public FileSerializer(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
     public void serialize(Map<String, Double> data) {
         //формирует результирующий json и сохраняет его в файл
+        try (var output = new BufferedWriter(new FileWriter(fileName))) {
+            var gson = new Gson();
+            output.write(gson.toJson(data));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- L16-io/homework/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ L16-io/homework/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,31 @@
 package ru.otus.dataprocessor;
 
+import com.google.common.io.Resources;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 import ru.otus.model.Measurement;
+
+import java.io.*;
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
 
+    private final String fileName;
+
     public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
     public List<Measurement> load() {
         //читает файл, парсит и возвращает результат
+        try {
+            Gson gson = new Gson();
+            JsonReader reader = new JsonReader(new FileReader(Resources.getResource(fileName).getFile()));
+            return List.of(gson.fromJson(reader, Measurement[].class));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
